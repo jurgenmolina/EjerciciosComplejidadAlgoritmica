aef2cb7a43df6c6c1b1c1506d7accf77b1386fea
Jurgen Molina <jurgenhumbertomoba@ufps.edu.co>
2022-04-04 20:16:30 -0500

    xd

---
+++ Ejercicios/src/CrearL3/L3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CrearL3;
+
+/**
+ *
+ * @author Jurgenmolina <jurgenmolina29@gmail.com>
+ */
+public class L3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Ejercicios/src/SumasN/SumasN.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SumasN;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jurgenmolina <jurgenmolina29@gmail.com>
+ */
+public class SumasN {
+
+    /**
+     * @param args the command line arguments
+     */
+    static ArrayList<String> sumas = new ArrayList();
+
+    public static void main(String[] args) {
+        System.out.println(sumasN(0));
+    }
+
+    public static String sumasN(int n) {
+        
+        if (n == 1) {
+            return "1";
+        }
+        
+        if (n>1) {
+            return "1+" + sumasN(n-1);
+        }
+        
+        return "";
+        
+    }
+
+}
