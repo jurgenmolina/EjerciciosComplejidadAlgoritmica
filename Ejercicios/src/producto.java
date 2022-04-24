5f37337581e9d95113200e1c60126496cd61306d
Jurgen Molina <jurgenhumbertomoba@ufps.edu.co>
2022-04-24 17:29:07 -0500

    acto acto pide contacto

---
+++ Ejercicios/src/producto.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jurgenmolina <jurgenmolina29@gmail.com>
+ */
+public class producto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        System.out.println("Producto de "+ a + "*" + b + " = " + producto(a, b));
+    }
+    
+    public static int producto(int a, int b){
+        int aux = 0;
+        
+        while (b>0) {            
+            aux += a;
+            b--;
+            
+        }
+        
+        return aux;
+        
+    }
+    
+}
